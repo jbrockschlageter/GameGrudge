bf07ac02e1c82cb67c15d99c8279b977406166ea
Jessica Brock-Schlageter <jrb6954@rit.edu>
2020-09-20 23:52:48 -0400

    test

--- src/TestJavaFXInstall/HelloFX.java
+++ src/TestJavaFXInstall/HelloFX.java
@@ -12,6 +12,9 @@ public class HelloFX extends Application {
 
     @Override
     public void start( Stage stage ) {
+
+        //add comment
+
         String javaVersion = System.getProperty( "java.version" );
         String javafxVersion = System.getProperty( "javafx.version" );
         Label l = new Label(
