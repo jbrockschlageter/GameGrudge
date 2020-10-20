e143a0ab73c315965ad620b0a8a086ae82f45a4d
gjf7128 <gjf7128@rit.edu>
2020-10-20 15:00:07 -0400

    Gabe added numPlayers to populate field method in State_teamFormation class

--- src/GameGrudge/States/State_TeamFormation.java
+++ src/GameGrudge/States/State_TeamFormation.java
@@ -9,6 +9,7 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
 public class State_TeamFormation implements GameGrudgeState{
+    public BorderPane sp;
     @Override
     public Scene constructStage(UIApplication app) {
         //Creating the scene
@@ -17,7 +18,7 @@ public class State_TeamFormation implements GameGrudgeState{
         //Add a label for buzz in buttons (team 1 = a ; team 2 = l)
 
         Label numPlayers = new Label("How many players are on each team?");
-        BorderPane sp = new BorderPane();
+        sp = new BorderPane();
         sp.setLeft(team1);
         sp.setRight(team2);
         sp.setCenter(numPlayers);
@@ -38,19 +39,19 @@ public class State_TeamFormation implements GameGrudgeState{
         VBox buttons = new VBox();
         Button twoPlayers = new Button("Two");
         twoPlayers.setOnAction(e-> {
-            populateField(app,2);
+            populateField(app,2, buttons);
         });
         Button threePlayers = new Button("Three");
         threePlayers.setOnAction(e-> {
-            populateField(app,3);
+            populateField(app,3, buttons);
         });  //Gabe was here.
         Button fourPlayers = new Button("Four");
         fourPlayers.setOnAction(e-> {
-            populateField(app,4);
+            populateField(app,4, buttons);
         });
         Button fivePlayers = new Button("Five");
         fivePlayers.setOnAction(e-> {
-            populateField(app,5);
+            populateField(app,5, buttons);
         });
         buttons.getChildren().add(twoPlayers);
         buttons.getChildren().add(threePlayers);
@@ -65,9 +66,26 @@ public class State_TeamFormation implements GameGrudgeState{
         return scene;
     }
 
-    public int populateField(UIApplication app, int numPlayers) {
-        TextField answer = new TextField();
-        return 0;
+    public int populateField(UIApplication app, int numPlayers, VBox buttons) {
+        TextField answer = new TextField("How many players?");
+        sp.getChildren().remove(buttons);
+        if (numPlayers == 2) {
+            app.gameModel.saveNames(2);
+
+        }
+        else if (numPlayers == 3) {
+            app.gameModel.saveNames(3);
+
+        }
+        else if (numPlayers == 4) {
+            app.gameModel.saveNames(4);
+
+        }
+        else {
+            app.gameModel.saveNames(5);
+
+        }
+        return 0; //Succesful function
 
     }
 }
