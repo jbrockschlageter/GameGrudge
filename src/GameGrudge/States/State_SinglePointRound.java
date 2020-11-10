bf809c4d29e4876cbe6b729f1f2a9ed981dfdea0
Jessica Brock-Schlageter <jrb6954@rit.edu>
2020-11-10 15:24:38 -0500

    Strike and Answer conditions are done, just need to switch teams or progress now.

--- src/GameGrudge/States/State_SinglePointRound.java
+++ src/GameGrudge/States/State_SinglePointRound.java
@@ -3,6 +3,7 @@ package GameGrudge.States;
 import GameGrudge.Question;
 import GameGrudge.UIApplication;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
@@ -12,6 +13,8 @@ import javafx.scene.layout.VBox;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
+import java.util.ArrayList;
+
 public class State_SinglePointRound implements GameGrudgeState{
 
     Question question;
@@ -19,7 +22,7 @@ public class State_SinglePointRound implements GameGrudgeState{
     Integer strikes = 0;
     Integer numAnswersGotten = 0;
 
-    //TODO: add strike and number of answer limits accordingly, and progress or switch teams accordingly.
+    //TODO: progress or switch teams accordingly.
 
     public State_SinglePointRound(Question q){
         importStates(q);
@@ -53,11 +56,17 @@ public class State_SinglePointRound implements GameGrudgeState{
             vb.getChildren().add(sp);
         }
 
+        for (String answer: question.answers.keySet()) {
+            if (question.answers.get(answer).get(1) == 1) {
+                submitAnswer(answer, vb, hbox,true);
+            }
+        }
+
         Scene scene = new Scene(vb);
 
         scene.setOnKeyPressed(e -> {
             if (e.getCode() == KeyCode.ENTER){
-                submitAnswer(textField.getCharacters().toString(), vb, hbox);
+                submitAnswer(textField.getCharacters().toString(), vb, hbox, false);
             }
         });
 
@@ -68,27 +77,44 @@ public class State_SinglePointRound implements GameGrudgeState{
         question = q;
     }
 
-    public boolean submitAnswer(String answer, VBox vb, HBox hb){
-        if(validateAnswer(answer)){
-            vb.getChildren().remove(numAnswersGotten + 1);
-            vb.getChildren().add(numAnswersGotten + 1, new Text(answer));
-            numAnswersGotten++;
-            app.refreshStage();
-            return true;
+    public boolean submitAnswer(String answer, VBox vb, HBox hb, boolean tossUpCase){
+        if(validateAnswer(answer, tossUpCase)){
+            boolean allAnswersFound = true;
+            for (String key : question.answers.keySet()) {
+                if(question.answers.get(key).get(1) != 1){
+                    allAnswersFound = false;
+                }
+            }
+
+            if(allAnswersFound){
+                endQuestion(true, vb, hb);
+            } else {
+                vb.getChildren().remove(numAnswersGotten + 1);
+                vb.getChildren().add(numAnswersGotten + 1, new Text(answer));
+                numAnswersGotten++;
+                app.refreshStage();
+                return true;
+            }
         }
         else{
             strikes++;
-            hb.getChildren().remove(3);
-            hb.getChildren().add(3, new Text(this.strikes.toString()));
-            app.refreshStage();
-            return false;
+
+            if(strikes >= 3){
+                endQuestion(false, vb, hb);
+            } else {
+                hb.getChildren().remove(3);
+                hb.getChildren().add(3, new Text(this.strikes.toString()));
+                app.refreshStage();
+                return false;
+            }
         }
+        return false;
     }
 
-    public boolean validateAnswer(String answer){
+    public boolean validateAnswer(String answer, boolean tossUpCase){
         answer = answer.toLowerCase().trim();
         if(question.answers.containsKey(answer)){
-            if(question.answers.get(answer).get(1) != 1) {
+            if(question.answers.get(answer).get(1) != 1 || tossUpCase) {
                 Integer pointValue = question.answers.get(answer).get(0);
                 app.gameModel.addPoints(question.possessingTeam, pointValue, 1);
                 question.answers.get(answer).set(1, 1);
@@ -96,5 +122,28 @@ public class State_SinglePointRound implements GameGrudgeState{
             }
         }
         return false;
+    }
+
+    public void endQuestion(boolean didWin, VBox vb, HBox hb){
+        vb.getChildren().remove(1,vb.getChildren().size());
+
+        hb.getChildren().remove(0, hb.getChildren().size());
+        Text endingMessage = new Text();
+        Button continueButton = new Button("Click here to continue to next question");
+        if(didWin){
+            endingMessage = new Text("Congratulations! Team " + question.possessingTeam + " has guessed all the answers!");
+            continueButton.setOnAction(e -> {
+
+            });
+        }
+        else{
+            endingMessage = new Text("Oh no! Team " + question.possessingTeam + " has gotten three strikes!");
+            continueButton.setOnAction(e -> {
+
+            });
+        }
+
+        hb.getChildren().add(endingMessage);
+        hb.getChildren().add(continueButton);
     }
 }
